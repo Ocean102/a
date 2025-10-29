8c7f93f16a8de99f67da309a6260437d8faed49a
Ocean102 <166802181+Ocean102@users.noreply.github.com>
2025-10-29 16:07:53 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,24 @@
-local hubDat = {
+local hub = {
     name = "Tide Hub",
     rf = false,
 
     key = {
-        enabled = false,
-        val = {}
+        enabled = true,
+        title = "Hệ thống key",
+        subtitle = "key super bí mật",
+        note = "trong discord",
+        file = "cool",
+        save = true,
+        val = { "khoinguyenamdau1234" }
+    },
+
+    colors = {
+        name = "#4a8fff",
+        error = "#ff3333"
     }
 }
 
-hubDat.notifyPrefix = '<font color="#4a8fff"><b>' .. hubDat.name .. '</b></font> - '
+hub.notifyPrefix = '<font color="' .. hub.colors.name .. '"><b>' .. hubDat.name .. '</b></font> - '
+hub.errorPrefix = '<font color="' .. hub.colors.name .. '"><b>Lỗi callback</b></font>: bấm f9, kép xuống để gửi lỗi'
+
 return hubDat
